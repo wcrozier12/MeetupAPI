f88f86034b68ee14e436f4cbeaa9609c9344d2de
wcrozier12 <wcc8@nau.edu>
2017-08-29 14:31:46 -0700

    w/ heroku

--- logic.js
+++ logic.js
@@ -5,9 +5,7 @@ function displayMeetUp() {
 var topic = 'javascript';
 var zip = '85254';
 var ApiKey = '1a143e3f55f5e4a64664065683536';
-var queryUrl = 'https://api.meetup.com/groups?key=' + ApiKey + '&sign=true&photo-host=public&topic=' + topic + '&zip=' + zip + '&page=3';
-
-
+var queryUrl = 'https://cfe-meetup-api.herokuapp.com/groups?key=' + ApiKey + '&sign=true&photo-host=public&topic=' + topic + '&zip=' + zip + '&page=3';
   $.ajax({
     
     url: queryUrl,
