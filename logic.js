1a909c37560a7ec468683b4cc091c4af7c989271
wcrozier12 <wcc8@nau.edu>
2017-08-29 14:04:12 -0700

    test

---
+++ logic.js
@@ -0,0 +1,20 @@
+
+
+
+function displayMeetUp() {
+var topic = 'javascript';
+var zip = '85254';
+var ApiKey = '1a143e3f55f5e4a64664065683536';
+var queryUrl = 'https://api.meetup.com/groups?key=' + ApiKey + '&sign=true&photo-host=public&topic=' + topic + '&zip=' + zip + '&page=3';
+
+
+  $.ajax({
+    
+    url: queryUrl,
+    method: 'GET'
+  }).done(function(response) {
+    console.log(response);
+  });
+
+};
+displayMeetUp();
